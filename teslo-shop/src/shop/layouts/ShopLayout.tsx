fb2f19b6ee0fee1c7b2f96b0701be39f5a9c0986
apiolli <cesardiaz.miduho@gmail.com>
2026-09-09 19:47:05 -0400

    feat: enhance layout and styling with custom header and input components

--- teslo-shop/src/shop/layouts/ShopLayout.tsx
+++ teslo-shop/src/shop/layouts/ShopLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import { CustomHeader } from "../components/CustomHeader";
 
 export const ShopLayout = () => {
   return (
-    <div>
-      ShopLayout
+    <div className="min-h-screen bg-background">
+      <CustomHeader />
       <Outlet />
     </div>
   );
--- teslo-shop/src/shop/pages/home/HomePage.tsx
+++ teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 export const HomePage = () => {
   return (
     <div>
-      HomePage
+      <h1>Hola mundo</h1>
+      <h1 className="font-montserrat">Hola mundo</h1>
       <Button>Hola mundo asdas</Button>
     </div>
   );
