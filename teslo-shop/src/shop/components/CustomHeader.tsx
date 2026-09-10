faac301a9641cebb19ce53512d605ca771ebdc5a
apiolli <cesardiaz.miduho@gmail.com>
2026-09-09 23:20:00 -0400

    feat: add search functionality to CustomHeader component

--- teslo-shop/src/shop/components/CustomHeader.tsx
+++ teslo-shop/src/shop/components/CustomHeader.tsx
@@ -1,10 +1,29 @@
 import { Search, ShoppingBag, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useSearchParams } from "react-router";
 
 export const CustomHeader = () => {
   const [cartCount] = useState(3);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const query = searchParams.get("query") || "";
+
+  const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== "Enter") return;
+
+    const query = inputRef.current?.value;
+
+    if (!query) return;
+
+    const newSearchParams = new URLSearchParams();
+    newSearchParams.set("query", query);
+    setSearchParams(newSearchParams);
+  };
+
   return (
     <header className="sticky top-0 z-50 w-full border-b backdrop-blur bg-slate-50">
       <div className="container mx-auto px-4 lg:px-8">
@@ -54,7 +73,9 @@ export const CustomHeader = () => {
                 <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
                 <Input
                   placeholder="Buscar productos..."
-                  className="pl-9 w-64 h-9"
+                  className="pl-9 w-64 h-9 bg-white"
+                  ref={inputRef}
+                  // onKeyDown={event => }
                 />
               </div>
             </div>
