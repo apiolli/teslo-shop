4b0abbf4c5cafde29574075806347ee15f74bd6a
apiolli <cesardiaz.miduho@gmail.com>
2026-09-12 19:12:56 -0400

    feat: implement authentication layout and enhance login/register pages with forms and UI components

--- teslo-shop/src/shop/components/CustomHeader.tsx
+++ teslo-shop/src/shop/components/CustomHeader.tsx
@@ -1,13 +1,12 @@
-import { Search, ShoppingBag, Menu } from "lucide-react";
+import { Search, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { Link, useParams, useSearchParams } from "react-router";
 import { cn } from "@/lib/utils";
+import { CustomLogo } from "@/components/custom/CustomLogo";
 
 export const CustomHeader = () => {
-  const [cartCount] = useState(3);
-
   const [searchParams, setSearchParams] = useSearchParams();
   const { gender } = useParams();
 
@@ -34,14 +33,7 @@ export const CustomHeader = () => {
       <div className="container mx-auto px-4 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
-          <div className="flex items-center space-x-4">
-            <Button variant="ghost" size="icon" className="md:hidden">
-              <Menu className="h-5 w-5" />
-            </Button>
-            <h1 className="text-xl font-semibold tracking-tight">
-              TESLA STYLE
-            </h1>
-          </div>
+          <CustomLogo />
 
           {/* Navigation - Desktop */}
           <nav className="hidden md:flex items-center space-x-8">
@@ -102,14 +94,17 @@ export const CustomHeader = () => {
               <Search className="h-5 w-5" />
             </Button>
 
-            <Button variant="ghost" size="icon" className="relative">
-              <ShoppingBag className="h-5 w-5" />
-              {cartCount > 0 && (
-                <span className="absolute -top-1 -right-1 h-5 w-5 rounded-full bg-primary text-primary-foreground text-xs flex items-center justify-center">
-                  {cartCount}
-                </span>
-              )}
-            </Button>
+            <Link to={"/auth/login"}>
+              <Button variant={"default"} size="sm" className={"ml-2"}>
+                Login
+              </Button>
+            </Link>
+
+            <Link to={"/admin"}>
+              <Button variant={"destructive"} size="sm">
+                Admin
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
