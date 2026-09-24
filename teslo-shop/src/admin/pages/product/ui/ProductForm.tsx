747e0907dd60e85202052b10cf89f69d6f9dc4ed
apiolli <cesardiaz.miduho@gmail.com>
2026-09-23 22:59:13 -0400

    feat: add react-hook-form for improved form handling in ProductForm component

--- teslo-shop/src/admin/pages/product/ui/ProductForm.tsx
+++ teslo-shop/src/admin/pages/product/ui/ProductForm.tsx
@@ -4,19 +4,20 @@ import type { Product } from "@/types/product.interface";
 import { X, SaveAll, Tag, Upload } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router";
-
+import { useForm } from "react-hook-form";
 interface Props {
   title: string;
   subTitle: string;
   product: Product;
 }
 
-const availableSizes = ["XS", "XS", "S", "M", "L", "XL", "XXL", "XXL"];
+const availableSizes = ["XS", "S", "M", "L", "XL", "XXL"];
 
 export const ProductForm = ({ title, subTitle, product }: Props) => {
-  console.log({ product });
-
   const [dragActive, setDragActive] = useState(false);
+  const { register } = useForm({
+    defaultValues: product,
+  });
 
   const addTag = () => {
     if (newTag.trim() && !product.tags.includes(newTag.trim())) {
@@ -111,6 +112,7 @@ export const ProductForm = ({ title, subTitle, product }: Props) => {
                     type="text"
                     // value={product.title}
                     // onChange={(e) => handleInputChange('title', e.target.value)}
+                    {...register("title")}
                     className="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
                     placeholder="Título del producto"
                   />
@@ -127,6 +129,7 @@ export const ProductForm = ({ title, subTitle, product }: Props) => {
                       // onChange={(e) =>
                       //   handleInputChange('price', parseFloat(e.target.value))
                       // }
+                      {...register("price")}
                       className="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
                       placeholder="Precio del producto"
                     />
@@ -142,6 +145,7 @@ export const ProductForm = ({ title, subTitle, product }: Props) => {
                       // onChange={(e) =>
                       //   handleInputChange('stock', parseInt(e.target.value))
                       // }
+                      {...register("stock")}
                       className="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
                       placeholder="Stock del producto"
                     />
@@ -156,6 +160,7 @@ export const ProductForm = ({ title, subTitle, product }: Props) => {
                     type="text"
                     // value={product.slug}
                     // onChange={(e) => handleInputChange('slug', e.target.value)}
+                    {...register("slug")}
                     className="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
                     placeholder="Slug del producto"
                   />
@@ -170,6 +175,7 @@ export const ProductForm = ({ title, subTitle, product }: Props) => {
                     // onChange={(e) =>
                     //   handleInputChange('gender', e.target.value)
                     // }
+                    {...register("gender")}
                     className="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
                   >
                     <option value="men">Hombre</option>
@@ -188,6 +194,7 @@ export const ProductForm = ({ title, subTitle, product }: Props) => {
                     // onChange={(e) =>
                     //   handleInputChange('description', e.target.value)
                     // }
+                    {...register("description")}
                     rows={5}
                     className="w-full px-4 py-3 border border-slate-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 resize-none"
                     placeholder="Descripción del producto"
